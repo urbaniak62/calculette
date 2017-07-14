056b285b25519a70dec2cf966415150de131cdca
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-14 19:08:10 +0200

    base html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var chiffre= document.getElementsByClassName=("button");
+var add= document.getElementsByClassName=("buttonAdd");
+var Moins= document.getElementsByClassName=("buttonMoins");
+var multi= document.getElementsByClassName=("buttonMulti");
+var div= document.getElementsByClassName=("buttonDiv");
+var result=document.getElementsByClassName=("buttonegal");
+var ecran=document.getElementById=("ecran");
+
+function calcul(a){
+	
+	console.log(a);
+	
+}
