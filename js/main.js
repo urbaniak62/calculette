cbb8b127bfa4e177bc1370883d0e0fb2a0376f1a
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-17 16:41:39 +0200

    ajout des fonction de calcul

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-var chiffre= document.getElementsByClassName=("button");
-var add= document.getElementsByClassName=("buttonAdd");
-var Moins= document.getElementsByClassName=("buttonMoins");
-var multi= document.getElementsByClassName=("buttonMulti");
-var div= document.getElementsByClassName=("buttonDiv");
-var result=document.getElementsByClassName=("buttonegal");
-var ecran=document.getElementById=("ecran");
+
+
+var chiffre= document.getElementsByClassName("button");
+// var add= document.getElementsByClassName("buttonAdd");
+var Moins= document.getElementsByClassName("buttonMoins");
+var multi= document.getElementsByClassName("buttonMulti");
+var div= document.getElementsByClassName("buttonDiv");
+// var resulta= document.getElementsByClassName("buttonegal");
+var ecran= document.getElementById("ecran");
 
 function calcul(a){
-	
-	console.log(a);
-	
-}
+	ecran.value= ecran.value +a ;
+
+ console.log(ecran.value);
+	}
+	 function add(a){
+		 ecran.value= ecran.value +a ;
+		console.log(ecran.value);
+	}
+
+	/*RESULTAT-----------------*/
+	function resulta(){
+		var total = eval(ecran.value);
+		alert(total);
+		document.getElementById("ecran").innerHTML=total;
+
+
+		// alert(eval(total.toString()));
+
+	}
