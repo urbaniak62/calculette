ec2344601932a281b448a1aea30341a7f363c72a
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-19 10:20:54 +0200

    ajout de la fonction clear

--- js/main.js
+++ js/main.js
@@ -7,20 +7,25 @@ var multi= document.getElementsByClassName("buttonMulti");
 var div= document.getElementsByClassName("buttonDiv");
 // var resulta= document.getElementsByClassName("buttonegal");
 var ecran= document.getElementById("ecran");
-
+var effacer="0";
 function calcul(a){
 	ecran.value= ecran.value +a ;
 
- console.log(ecran.value);
+ //console.log(ecran.value);
 	}
 	 function add(a){
 		 ecran.value= ecran.value +a ;
-		console.log(ecran.value);
+		//console.log(ecran.value);
 	}
+
 
 	/*RESULTAT-----------------*/
 	function resulta(){
 
 		ecran.value=eval(ecran.value);
 	}
-		
+
+
+	function toto(){
+	ecran.value=ecran.value.slice(0,ecran.value.length-1);
+	}
