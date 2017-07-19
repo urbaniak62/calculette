c147d497a50ce525c1aa4d8ba3d477ec7e9c2689
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-19 10:49:24 +0200

    ajout de la fonction clear all

--- js/main.js
+++ js/main.js
@@ -18,14 +18,20 @@ function calcul(a){
 		//console.log(ecran.value);
 	}
 
-
 	/*RESULTAT-----------------*/
 	function resulta(){
-
 		ecran.value=eval(ecran.value);
-	}
+}
 
+/*-----------EFFACER-UN-CHIFFRE-A-LA-FOIS-------*/
 
 	function toto(){
 	ecran.value=ecran.value.slice(0,ecran.value.length-1);
 	}
+
+	/*-----------EFFACER-TOUS-L'ECRAN-------*/
+
+	function totoA(){
+		ecran.value=ecran.value.slice(0,ecran.value.length-ecran.value.length);
+	}
+/*------------FIN-DE--SCRIPT----------*/
